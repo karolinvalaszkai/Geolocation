1a6fcbef3683531aae67395b141efc8cd5fc99ae
Sabina Nordell <sabinanordell@n188-p198.eduroam.kth.se>
2018-04-09 13:57:59 +0200

    s
    s

--- controllers.js
+++ controllers.js
@@ -1,5 +1,7 @@
-var map;
-var chicago = {lat: 41.85, lng: -87.65};
+var map; 
+var zoomRate = 17;
+
+var KTH = {lat: 59.3498092, lng: 18.0684758};
 
 /**
  * The CenterControl adds a control to the map that recenters the map on
@@ -7,7 +9,7 @@ var chicago = {lat: 41.85, lng: -87.65};
  * This constructor takes the control DIV as an argument.
  * @constructor
  */
-function CenterControl(controlDiv, map) {
+function ZoomIn(controlDiv, map) {
 
   // Set CSS for the control border.
   var controlUI = document.createElement('div');
@@ -34,25 +36,100 @@ function CenterControl(controlDiv, map) {
 
   // Setup the click event listeners: simply set the map to Chicago.
   controlUI.addEventListener('click', function() {
-  	map.zoom = map.zoom + 1
-    console.log(map.zoom)
-    initMap() 
+  	zoomRate = map.getZoom() + 1;
+    initMap(map); 
   });
 
 }
 
-function initMap() {
+function ZoomOut(controlDiv, map) {
+
+  // Set CSS for the control border.
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = '#fff';
+  controlUI.style.border = '2px solid #fff';
+  controlUI.style.borderRadius = '3px';
+  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.marginBottom = '22px';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'Click to zoom!';
+  controlDiv.appendChild(controlUI);
+
+  // Set CSS for the control interior.
+  var controlText = document.createElement('div');
+  controlText.style.color = 'rgb(25,25,25)';
+  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlText.style.fontSize = '16px';
+  controlText.style.lineHeight = '38px';
+  controlText.style.paddingLeft = '5px';
+  controlText.style.paddingRight = '5px';
+  controlText.innerHTML = '-';
+  controlUI.appendChild(controlText);
+
+  // Setup the click event listeners: simply set the map to Chicago.
+  controlUI.addEventListener('click', function() {
+  	zoomRate = map.getZoom() - 1;
+    initMap(map); 
+  });
+
+}
+
+function PanRight(controlDiv, map) {
+
+  // Set CSS for the control border.
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = '#fff';
+  controlUI.style.border = '2px solid #fff';
+  controlUI.style.borderRadius = '3px';
+  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.marginBottom = '22px';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'Click to zoom!';
+  controlDiv.appendChild(controlUI);
+
+  // Set CSS for the control interior.
+  var controlText = document.createElement('div');
+  controlText.style.color = 'rgb(25,25,25)';
+  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlText.style.fontSize = '16px';
+  controlText.style.lineHeight = '38px';
+  controlText.style.paddingLeft = '5px';
+  controlText.style.paddingRight = '5px';
+  controlText.innerHTML = '>';
+  controlUI.appendChild(controlText);
+
+  // Setup the click event listeners: simply set the map to Chicago.
+  controlUI.addEventListener('click', function() {
+  	KTH.lat = KTH.lng;
+  	console.log(KTH.lng,map.getCenter())
+
+    initMap(map) 
+  });
+
+}
+
+
+function initMap(map) {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 59.3498092, lng: 18.0684758},
-    zoom: 17,
+    center: KTH,
+    zoom: zoomRate,
     zoomControl: false
   });
 
   // Create the DIV to hold the control and call the CenterControl()
   // constructor passing in this DIV.
-  var centerControlDiv = document.createElement('div');
-  var centerControl = new CenterControl(centerControlDiv, map);
+  var leftControlDiv = document.createElement('div');
+  var ZoomInControl = new ZoomIn(leftControlDiv, map);
+  var ZoomOutControl = new ZoomOut(leftControlDiv, map);
+  var PanRightControl = new PanRight(leftControlDiv, map);
 
-  centerControlDiv.index = 1;
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
+
+  leftControlDiv.index = 1;
+  map.controls[google.maps.ControlPosition.LEFT_CENTER].push(leftControlDiv);
+
+
+
+
 }
