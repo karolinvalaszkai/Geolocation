9a6efa17c842ac72c085cc47cd3eb74ce12f5f1a
Karolin Valaszkai <karolinv@kth.se>
2018-04-11 14:39:12 +0200

    k

--- js/controllers.js
+++ js/controllers.js
@@ -3,43 +3,7 @@ if(navigator.standalone == true) {
   console.log("installed")
 }
 
-<<<<<<< HEAD
-jQuery(window).ready(function(){
-            jQuery("#btnInit").click(initiate_geolocation);
-        });
 
-        function initiate_geolocation() {
-
-            navigator.geolocation.getCurrentPosition(handle_geolocation_query,handle_errors);
-        }
-
-        function handle_errors(error)
-        {
-            switch(error.code)
-            {
-                case error.PERMISSION_DENIED: alert("user did not share geolocation data");
-                break;
-
-                case error.POSITION_UNAVAILABLE: alert("could not detect current position");
-                break;
-
-                case error.TIMEOUT: alert("retrieving position timed out");
-                break;
-
-                default: alert("unknown error");
-                break;
-            }
-        }
-
-        function handle_geolocation_query(position){
-            alert('Lat: ' + position.coords.latitude +
-                  ' Lon: ' + position.coords.longitude);
-            //console.log('Finding location');
-            //location = {lat: position.coords.latitude, lng: position.coords.longitude};
-        }
-=======
-
->>>>>>> 245983e579135b28261df0fbfaadf2386e24f724
 
   var map;
   var zoomRate = 17;
