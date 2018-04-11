2d22b182818cdc7b8710c6ce4f19fc37889f8481
Sabina Nordell <sabinanordell@n184-p136.eduroam.kth.se>
2018-04-11 14:04:12 +0200

    s
    s

--- js/controllers.js
+++ js/controllers.js
@@ -3,39 +3,7 @@ if(navigator.standalone == true) {
   console.log("installed")
 }
 
-jQuery(window).ready(function(){
-            jQuery("#btnInit").click(initiate_geolocation);
-        });
- 
-        function initiate_geolocation() {
 
-            navigator.geolocation.getCurrentPosition(handle_geolocation_query,handle_errors);
-        }
- 
-        function handle_errors(error)
-        {
-            switch(error.code)
-            {
-                case error.PERMISSION_DENIED: alert("user did not share geolocation data");
-                break;
- 
-                case error.POSITION_UNAVAILABLE: alert("could not detect current position");
-                break;
- 
-                case error.TIMEOUT: alert("retrieving position timed out");
-                break;
- 
-                default: alert("unknown error");
-                break;
-            }
-        }
- 
-        function handle_geolocation_query(position){
-            alert('Lat: ' + position.coords.latitude +
-                  ' Lon: ' + position.coords.longitude);
-            //console.log('Finding location');
-            //location = {lat: position.coords.latitude, lng: position.coords.longitude};
-        }
 
   var map;
   var zoomRate = 17;
