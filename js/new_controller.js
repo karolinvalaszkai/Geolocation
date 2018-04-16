67f69a550b9deba79b7fab919d55fb59a62bc722
Sabina Nordell <sabinanordell@n146-p152.eduroam.kth.se>
2018-04-16 10:37:05 +0200

    s
    s

--- js/new_controller.js
+++ js/new_controller.js
@@ -2,6 +2,7 @@
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
+console.log('hej')
 var map, infoWindow;
 var KTH = {lat: 59.3498092, lng: 18.0684758};
 var zoomRate = 17;
